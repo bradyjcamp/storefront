6f38bc64cc32a132711570bb11803bc1ffdb6abe
Brady Camp <bradyjcamp@gmail.com>
2022-05-09 16:08:20 -0400

    class demo

---
+++ src/component/candidateList/CandidateList.js
@@ -0,0 +1,55 @@
+import { connect } from 'react-redux';
+import Card from '@mui/material/Card';
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
+import Button from '@mui/material/Button';
+import IconButton from '@mui/material/IconButton';
+import DeleteIcon from '@mui/icons-material/Delete';
+import Typography from '@mui/material/Typography';
+import Box from '@mui/material/Box';
+import { incrementCount, decrementCount, reset } from '../../store/votes.js';
+
+
+function CandidateList({ totalVotes, candidates, incrementCount, decrementCount, reset }) {
+
+  console.log(totalVotes);
+  console.log(candidates);
+
+  return(
+    <>
+    <Box sx={{display: 'flex', flexDirection: 'row', justifyContent: 'center'}}id="list">
+      {candidates.map(candidate => (
+          <Card sx={{ margin: "10px" }} raised key={candidate.id}>
+            <CardContent>
+            <Typography gutterBottom variant="h3">{candidate.name}</Typography>
+            <Typography variant="body2" color="text.secondary">Votes: {candidate.votes}</Typography>
+            </CardContent>
+            <CardActions>
+            <Button onClick={() => incrementCount(candidate)}>Vote for {candidate.name}</Button>
+            <Button onClick={() => decrementCount(candidate)}>Un-Vote for {candidate.name}</Button>
+            </CardActions>
+          </Card>
+      ))}
+    </Box>
+      <IconButton onClick={() => reset()}>
+        <DeleteIcon />
+      </IconButton>
+    </>
+  )
+}
+
+const mapStateToProps = ({ votes }) => {
+  return {
+    totalVotes: votes.totalVotes,
+    candidates: votes.candidates
+  }
+}
+
+const mapDispatchToProps = {
+  incrementCount,
+  decrementCount,
+  reset
+}
+
+
+export default connect(mapStateToProps, mapDispatchToProps)(CandidateList);
