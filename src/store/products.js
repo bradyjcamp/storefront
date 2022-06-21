81c461f396388671bc90758b41996afa89df0e0d
Brady Camp <bradyjcamp@gmail.com>
2022-06-20 17:53:54 -0700

    deleted uneeded comments

--- src/store/products.js
+++ src/store/products.js
@@ -1,40 +1,7 @@
 import axios from 'axios';
 
 const initialState = {
-  products: [
-    // {
-    //   id: 1,
-    //   name: 'Tent',
-    //   category: 'outdoor-equipment',
-    //   description: 'A sturdy 10 x 10 tent for mild weather',
-    //   price: 50,
-    //   inventory: 15
-    // },
-    // {
-    //   id: 2,
-    //   name: 'Backpack',
-    //   category: 'outdoor-equipment',
-    //   description: 'A 50 Liter Backpack',
-    //   price: 150,
-    //   inventory: 5
-    // },
-    // {
-    //   id: 3,
-    //   name: 'Boots',
-    //   category: 'clothing',
-    //   description: 'Hiking Boots',
-    //   price: 95,
-    //   inventory: 10
-    // },
-    // {
-    //   id: 4,
-    //   name: 'Hiking Pants',
-    //   category: 'clothing',
-    //   description: 'Weatherproof rugged material',
-    //   price: 49,
-    //   inventory: 20
-    // }
-  ],
+  products: [],
   selectedProducts: [],
   productSelected: []
 }
