e4d1ef612dfe61aba0745fb457f4227941621cdc
Brady Camp <bradyjcamp@gmail.com>
2022-08-29 16:02:37 -0700

    added app bar from mui to header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,32 +1,46 @@
 // import React, { useState } from 'react';
-import { useSelector } from 'react-redux';
-import { Badge } from '@mui/material';
-import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
-import { Link } from 'react-router-dom';
-
+import { useSelector } from "react-redux";
+import { Badge } from "@mui/material";
+import ShoppingCartIcon from "@mui/icons-material/ShoppingCart";
+import { Link } from "react-router-dom";
+import * as React from "react";
+import AppBar from "@mui/material/AppBar";
+import Box from "@mui/material/Box";
+import Toolbar from "@mui/material/Toolbar";
+import Typography from "@mui/material/Typography";
+import IconButton from "@mui/material/IconButton";
+import HomeIcon from "@mui/icons-material/Home";
 
 const Header = (props) => {
-  
-  // const [showCart, setShowCart] = useState(false);
-  let cart = useSelector(state => state.cart)
-  
-  // const handleClick = () => {
-  //   setShowCart(!showCart)
-  // }
+  let cart = useSelector((state) => state.cart);
 
-  return(
-    <>
-      <div id='header'>
-        <header>Brady's Store</header>
-      </div>
-      <Link to="/storefront">Home</Link>
-      <Link to="/cart">
-        <Badge badgeContent={cart.itemCounter} color="primary">
-          <ShoppingCartIcon />
-        </Badge>
-      </Link>
-    </>
-  )
-}
+  return (
+    <Box id="header" sx={{ flexGrow: 1 }}>
+      <AppBar position="static" color="inherit">
+        <Toolbar>
+          <Link to="/storefront">
+            <IconButton
+              size="large"
+              edge="start"
+              color="inherit"
+              aria-label="home"
+              sx={{ mr: 2 }}
+            >
+              <HomeIcon />
+            </IconButton>
+          </Link>
+          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+            Brady's Store
+          </Typography>
+          <Link to="/cart">
+            <Badge badgeContent={cart.itemCounter} color="primary">
+              <ShoppingCartIcon />
+            </Badge>
+          </Link>
+        </Toolbar>
+      </AppBar>
+    </Box>
+  );
+};
 
-export default Header
+export default Header;
